159214cac237a82aa4fd7d010f6fa5c03ce3d2e0
akshai <57423537+nyouibou@users.noreply.github.com>
2024-04-14 15:21:15 +0530

    app updates

--- lib/controller/homescreencontrolle.dart
+++ lib/controller/homescreencontrolle.dart
@@ -4,18 +4,36 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
 class HomeScreenController extends GetxController {
-  num balance = 0;
-  num income = 0;
-  num expense = 0;
-  // var collection = FirebaseFirestore.instance.collection("transaction");
+  Rx<num> balance = 0.0.obs;
+  Rx<num> income = 0.0.obs;
+  Rx<num> expense = 0.0.obs;
+
   calculate(List<QueryDocumentSnapshot> entireData) {
-    income = 0;
-    expense = 0;
+    income.value = 0.0;
+    expense.value = 0.0;
+    // get income and expense list
     var incomeList =
         entireData.where((element) => element["type"] == "income").toList();
     var expenseList =
-        entireData.where((element) => element["type"] == "income").toList();
+        entireData.where((element) => element["type"] == "expense").toList();
+
     log(incomeList.length.toString());
     log(expenseList.length.toString());
+
+    // calculate total income
+
+    for (var element in incomeList) {
+      income.value += num.parse(element["amount"].toString());
+    }
+
+    log("income :${income.toString()}");
+
+    // calculate total expense
+    for (var element in expenseList) {
+      expense.value += num.parse(element["amount"].toString());
+    }
+    log("expense :${expense.toString()}");
+    balance.value = income.value - expense.value;
+    log("balance :${balance.toString()}");
   }
 }
--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -1,12 +1,12 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:expense_tracker/controller/homescreencontrolle.dart';
 import 'package:expense_tracker/view/addamount/addamount.dart';
-import 'package:expense_tracker/view/home_screen/widgets/income.dart';
 import 'package:expense_tracker/view/home_screen/widgets/total_in_ex_container.dart';
 import 'package:expense_tracker/view/home_screen/widgets/trancaction_card.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -17,81 +17,123 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   var collection = FirebaseFirestore.instance.collection("transaction");
-  HomeScreenController controller = HomeScreenController();
+
+  late HomeScreenController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = Get.put(HomeScreenController());
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-      ),
-      body: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-            decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius:
-                    BorderRadius.vertical(bottom: Radius.circular(10))),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Balance",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 25,
-                      fontWeight: FontWeight.w500),
-                ),
-                Text(
-                  "₹10000",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold),
-                ),
-                Row(
-                  children: [
-                    Income(),
-                    SizedBox(
-                      width: 16,
+    return SafeArea(
+      child: Scaffold(
+        body: StreamBuilder(
+          stream: collection.orderBy("date", descending: true).snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final List<QueryDocumentSnapshot> documents = snapshot.data!.docs;
+
+              controller.calculate(documents);
+              return Column(
+                children: [
+                  Obx(() => Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                      decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.vertical(
+                              bottom: Radius.circular(10))),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              CircleAvatar(
+                                radius: 20,
+                                backgroundImage: NetworkImage(
+                                    "https://i.pinimg.com/236x/82/98/7b/82987b434ac12cf32e1311f5b1d0cf24.jpg"),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Text(
+                                "Hi Eve!",
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.white),
+                              ),
+                              new Spacer(), // I just added one line
+                              InkWell(
+                                  onTap: () {},
+                                  child: Icon(Icons.menu, color: Colors.white))
+                            ],
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            "Balance",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 25,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            "₹${controller.balance.value}",
+                            style: TextStyle(
+                                color: Colors.blue,
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Row(
+                            children: [
+                              TotalExInContainer(
+                                amount: controller.income.value,
+                                textColor: Colors.green,
+                                title: "Income",
+                              ),
+                              SizedBox(
+                                width: 16,
+                              ),
+                              TotalExInContainer(
+                                amount: controller.expense.value,
+                                textColor: Colors.red,
+                                title: "Expense",
+                              ),
+                            ],
+                          )
+                        ],
+                      ))),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        DocumentSnapshot transactionData =
+                            snapshot.data!.docs[index];
+                        return TransactionCard(
+                          transactionData: transactionData,
+                        );
+                      },
                     ),
-                    TotalExInContainer(),
-                  ],
-                )
-              ],
-            ),
-          ),
-          Expanded(
-              child: StreamBuilder(
-            stream: collection.orderBy("date").snapshots(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return ListView.builder(
-                  itemCount: snapshot.data!.docs.length,
-                  itemBuilder: (context, index) {
-                    controller.calculate(snapshot.data!.docs);
-                    DocumentSnapshot transactionData =
-                        snapshot.data!.docs[index];
-                    return TransactionCard(
-                      transactionData: transactionData,
-                    );
-                  },
-                );
-              } else {
-                return CircularProgressIndicator();
-              }
-            },
-          ))
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => AddAmountScreen(),
-              ));
-        },
+                  ),
+                ],
+              );
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => AddAmountScreen(),
+                ));
+          },
+        ),
       ),
     );
   }
